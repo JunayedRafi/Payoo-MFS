5da0408d01e34b8fa9a5e4db09f26118d2d391ce
JunayedRafi <junayedrafi8@gmail.com>
2026-04-09 21:52:35 +0600

    Add Money, Cashout Added with functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 // Login Button Function
+document.getElementById('loginBtn').addEventListener('click', function(e){
+    e.preventDefault(); 
+    const mobileNumber = 12345;
+    const pinNumber = 1234;
+
+    const mobileNumberValaue = document.getElementById('mobileNumber').value;
+    const mobileNumberValueConverted = parseInt(mobileNumberValaue);
+    const pinNumberValue = document.getElementById('pinNumber').value;
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        
+    }
+    else{
+        alert('Login Failed');
+    }
+});
